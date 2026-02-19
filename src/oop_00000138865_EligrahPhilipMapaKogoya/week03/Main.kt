a3805366ebdaa8089b45c9aa0ef3fd766f4b5813
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-02-19 17:31:43 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
@@ -2,7 +2,6 @@ package oop_00000138865_EligrahPhilipMapaKogoya.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Basic Employee ---")
-    println("Nama: ${e.name}")
-    println("Gaji: ${e.salary}")
+    println("--- Test Error ---")
+    e.salary = 5000 // Ini akan meledakkan program Anda
 }
