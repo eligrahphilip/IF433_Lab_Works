dfd3ceabb0302a1b6d1f4ca299fe0d88756a28d7
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-02-19 17:36:50 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
@@ -1,6 +1,7 @@
 package oop_00000138865_EligrahPhilipMapaKogoya.week03
 
 fun main() {
+    println("=== TEST EMPLOYEE ===")
     val e = Employee("Budi")
     
     e.salary = -1000
@@ -10,4 +11,16 @@ fun main() {
     e.increasePerformance()
     
     println("Bonus yang harus dibayar: ${e.bonus}")
+    
+    println("\n=== TEST WEAPON ===")
+    val weapon = Weapon("Excalibur")
+    
+    println("Weapon: ${weapon.name}")
+    weapon.damage = -50
+    println("Damage setelah set -50: ${weapon.damage}")
+    
+    weapon.damage = 9999
+    println("Damage setelah set 9999: ${weapon.damage}")
+    
+    println("Tier: ${weapon.tier}")
 }
