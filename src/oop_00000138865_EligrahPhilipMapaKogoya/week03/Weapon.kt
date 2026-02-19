1b41855826140818a87d7dcb25194cfde63782de
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-02-19 17:35:53 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Weapon.kt
@@ -0,0 +1,28 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week03
+
+class Weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("WARNING: Damage tidak boleh negatif! Nilai tidak diubah.")
+                }
+                value > 1000 -> {
+                    println("WARNING: Damage terlalu tinggi! Di-set ke 1000.")
+                    field = 1000
+                }
+                else -> {
+                    field = value
+                }
+            }
+        }
+    
+    val tier: String
+        get() {
+            return when {
+                damage > 800 -> "Legendary"
+                damage > 500 -> "Epic"
+                else -> "Common"
+            }
+        }
+}
