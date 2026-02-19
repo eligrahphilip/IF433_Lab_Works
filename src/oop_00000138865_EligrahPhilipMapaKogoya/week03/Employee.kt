c2846959a4e37d4892f55dd33dae133103a000ea
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-02-19 17:33:25 +0700

    week03: add private property

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Employee.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Employee.kt
@@ -5,9 +5,27 @@ class Employee(val name: String) {
         set(value) {
             if (value < 0) {
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0 // Gunakan field, bukan this.salary
+                field = 0
             } else {
-                field = value // Gunakan field untuk assign nilai asli
+                field = value
             }
         }
+    
+    // Hanya bisa diakses di file/class ini saja
+    private var performanceRating: Int = 3
+    
+    fun increasePerformance() {
+        if (performanceRating < 5) {
+            performanceRating++
+            println("Kinerja $name meningkat! Rating: $performanceRating")
+        } else {
+            println("Rating sudah maksimal!")
+        }
+    }
+    
+    // Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar rahasia
+    // Kecuali kita buat function khusus untuk print.
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
