e5ca523a738345d8308645a6850daeedfaa4017f
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-02-19 17:29:59 +0700

    week03: create basic Employee class

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Employee.kt
@@ -0,0 +1,5 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week03
+
+class Employee(val name: String) {
+    var salary: Int = 0
+}
---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
@@ -0,0 +1,8 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Basic Employee ---")
+    println("Nama: ${e.name}")
+    println("Gaji: ${e.salary}")
+}
