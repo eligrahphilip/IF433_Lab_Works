c9b1a18cd8bd01a5133e41585ed528915e0f1a6c
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-02-19 17:32:42 +0700

    week03: fix setter using backing field

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week03/Main.kt
@@ -2,6 +2,9 @@ package oop_00000138865_EligrahPhilipMapaKogoya.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000 // Ini akan meledakkan program Anda
+    
+    // 1. Test Validasi Salary
+    e.salary = -1000 // Harusnya print error
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
 }
