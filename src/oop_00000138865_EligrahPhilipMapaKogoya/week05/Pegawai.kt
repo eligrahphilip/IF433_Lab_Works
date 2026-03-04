05df1fa9458e48953d6b2f3b3570d66a5c88ec95
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-03-04 10:48:30 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week05/Pegawai.kt
@@ -0,0 +1,8 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week05
+
+// Abstract class tidak bisa dibuat objeknya secara langsung
+abstract class Pegawai(val nama: String) {
+    
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
