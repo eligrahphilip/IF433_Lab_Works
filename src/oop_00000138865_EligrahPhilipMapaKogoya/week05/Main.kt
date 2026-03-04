a1f0034dfd1c0e1d27b1c9ab0bdde436d9470398
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-03-04 10:55:39 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week05/Main.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week05/Main.kt
@@ -16,7 +16,7 @@ fun main() {
         when (pegawai) {
             is Dosen -> {
                 println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                pegawai.mengajar() // Smart cast! Tidak perlu manual casting (as)
+                pegawai.mengajar()
             }
             is Admin -> {
                 println("=> Terdeteksi sebagai Admin")
@@ -25,4 +25,16 @@ fun main() {
         }
         println("------------------------")
     }
+    
+    println("\n=== TEST OVERLOADING (MathHelper) ===")
+    val math = MathHelper()
+    
+    val luasPersegi = math.hitungLuas(5)
+    println("Luas persegi (sisi 5): $luasPersegi")
+    
+    val luasPersegiPanjang = math.hitungLuas(10, 5)
+    println("Luas persegi panjang (10 x 5): $luasPersegiPanjang")
+    
+    val luasLingkaran = math.hitungLuas(7.0)
+    println("Luas lingkaran (jari-jari 7.0): $luasLingkaran")
 }
