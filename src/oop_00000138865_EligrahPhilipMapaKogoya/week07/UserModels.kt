bb3539b565b4764b83f35bfcb3200699074bb9b3
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-03-30 20:35:19 +0700

    week07: (trial) demonstrate regular class equality failure

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week07/UserModels.kt
@@ -0,0 +1,7 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week07
+
+// CHECKPOINT 5: (trial) demonstrate regular class equality failure
+class RegularUser(val name: String, val age: Int)
+
+// CHECKPOINT 6: implement data class for structural equality
+data class DataUser(val name: String, val age: Int)
