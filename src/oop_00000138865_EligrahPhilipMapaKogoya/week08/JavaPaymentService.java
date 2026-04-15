49fec873d3d3acc67b63d4961421408b080e8212
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-04-15 11:01:43 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
