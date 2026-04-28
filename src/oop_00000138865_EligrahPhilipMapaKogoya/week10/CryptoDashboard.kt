00c4962110bb81216decaa07f178b10f6e989804
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-04-29 00:33:20 +0700

    week10: (task) populate coin repository

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week10/CryptoDashboard.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000138865_EligrahPhilipMapaKogoya.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 3.2))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
