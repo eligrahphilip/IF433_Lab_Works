5995684d7534c11119f0088b1c58519816736011
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-04-29 00:31:15 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week10/WalletRepository.kt
@@ -0,0 +1,17 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+    fun search(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
+    }
+}
