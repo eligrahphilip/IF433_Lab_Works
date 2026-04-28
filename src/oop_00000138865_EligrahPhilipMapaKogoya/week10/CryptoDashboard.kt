5138f6c3a4d8cd6df0cf61d6632b23e8369772f3
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-04-29 00:35:22 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week10/CryptoDashboard.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week10/CryptoDashboard.kt
@@ -13,4 +13,9 @@ fun main() {
     response.data.forEach { coin ->
         println("Koin: ${coin.name}, Saldo: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 500.0))
+    txRepo.add(Transaction("TX002", 1200.5))
+    txRepo.add(Transaction("TX003", 75.0))
 }
