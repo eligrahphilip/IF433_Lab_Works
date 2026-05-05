18c2371ae9fcdc0dda8f5e3858a0e60b7e013995
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-05-05 22:24:21 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week11/SmartHomeDashboard.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week11/SmartHomeDashboard.kt
@@ -2,4 +2,12 @@ package oop_00000138865_EligrahPhilipMapaKogoya.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    // Konfigurasi Pencahayaan: apply untuk setup, also untuk tambah ke list
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
