1f0945335a2eba3c8b4513e477b30c01a3ff2ad5
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-05-05 22:23:07 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000138865_EligrahPhilipMapaKogoya.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
