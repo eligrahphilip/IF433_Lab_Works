553ea99796ad498ab09f8a566e7f647a6f2ce964
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-05-12 21:11:16 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week12/SmartFeeder.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week12/SmartFeeder.kt
@@ -36,5 +36,8 @@ fun main() {
     println("\n=== JADWAL MAKAN SORE ===")
     val eveningResult = runCatching {
         dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
