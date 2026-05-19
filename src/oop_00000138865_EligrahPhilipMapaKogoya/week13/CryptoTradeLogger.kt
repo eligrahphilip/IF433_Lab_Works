ea75cf52ccce6c412210adb2e5a9d36a63fc7642
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-05-19 20:27:30 +0700

    week13: (task) wrap fromCsvTrade in try-catch for NumberFormatException

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week13/CryptoTradeLogger.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week13/CryptoTradeLogger.kt
@@ -10,12 +10,17 @@ fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
 
 // Deserialization: CSV string -> TradeRecord? (nullable)
 fun fromCsvTrade(line: String): TradeRecord? {
-    val parts = line.split(",")
-    return TradeRecord(
-        id = parts[0].toInt(),
-        symbol = parts[1],
-        type = parts[2],
-        margin = parts[3].toDouble(),
-        pnl = parts[4].toDouble()
-    )
+    return try {
+        val parts = line.split(",")
+        TradeRecord(
+            id = parts[0].toInt(),
+            symbol = parts[1],
+            type = parts[2],
+            margin = parts[3].toDouble(),
+            pnl = parts[4].toDouble()
+        )
+    } catch (e: Exception) {
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
 }
