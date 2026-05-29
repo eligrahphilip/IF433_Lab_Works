827de49d7542d52206cfe8887eb1488e4402a85e
eligrahphilip <eligrahphilipmapakogoya@gmail.com>
2026-05-29 13:37:34 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000138865_EligrahPhilipMapaKogoya/week14/EcommerceTask.kt
+++ src/oop_00000138865_EligrahPhilipMapaKogoya/week14/EcommerceTask.kt
@@ -24,3 +24,35 @@ class BadOrderProcessor {
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
     }
 }
+
+// Fix SRP & DIP: pisahkan tanggung jawab ke interface
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class CsvOrderRepository : OrderRepository {
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        File("orders_safe.csv").printWriter().use { writer ->
+            writer.println("$itemName,$finalPrice,$customerType")
+        }
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(itemName: String) {
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
+class SafeOrderProcessor(val repo: OrderRepository, val notifier: NotificationService) {
+    fun processOrder(itemName: String, basePrice: Double, pricingStrategy: PricingStrategy) {
+        val finalPrice = pricingStrategy.calculate(basePrice)
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder(itemName, finalPrice, pricingStrategy.javaClass.simpleName)
+        notifier.sendNotification(itemName)
+    }
+}
